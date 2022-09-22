e91cf7bd1a64d9e6051c101cd8aff7895f33c745
Rodrigo Assis <rodrigomosquitu@gmail.com>
2022-09-22 13:44:07 -0300

    Criado arquivo bat para instalação de dependências

---
+++ install.bat
@@ -0,0 +1,51 @@
+REM @echo off
+
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+	echo This setup needs admin permissions. Please run this file as admin.
+	pause
+	exit
+)
+
+REM set NODE_EXEC=node-v0.8.11-x86.msi
+REM set SETUP_DIR=%CD%
+
+set NODE_VER=null
+node -v >tmp.txt
+set /p NODE_VER=<tmp.txt
+del tmp.txt
+echo %NODE_VER%
+
+SET CUR_DIR=%~dp0
+cd %CUR_DIR%
+mkdir zipados
+
+echo ---------------------------------------------------
+echo ----- Compiling and zipping CREATE CAMPAIGN Lambda...
+echo ---------------------------------------------------
+
+cd %CUR_DIR%\CreateCampaign
+call npm install
+call tar.exe -a -cf createcampaign.zip index.js node_modules package.json
+move createcampaign.zip %CUR_DIR%\zipados\
+
+echo ---------------------------------------------------
+echo ----- Compiling and zipping GET CAMPAIGN Lambda...
+echo ---------------------------------------------------
+
+cd %CUR_DIR%\GetCampaign
+call npm install
+call tar.exe -a -cf getcampaign.zip index.js node_modules package.json
+move getcampaign.zip %CUR_DIR%\zipados\
+
+echo ---------------------------------------------------
+echo ----- Terraform scripts
+echo ---------------------------------------------------
+
+cd %CUR_DIR%\terraform
+call terraform init
+
+echo DONE!
+pause
+
+
--- terraform/modules/api_gateway/api_gateway_integration_response.tf
+++ terraform/modules/api_gateway/api_gateway_integration_response.tf
@@ -10,7 +10,8 @@ resource "aws_api_gateway_integration_response" "tfer--k25cdk06uc-002F-eh1nmc-00
   status_code = "301"
 
   depends_on = [
-    aws_api_gateway_integration.tfer--k25cdk06uc-002F-eh1nmc-002F-GET
+    aws_api_gateway_integration.tfer--k25cdk06uc-002F-eh1nmc-002F-GET,
+    aws_api_gateway_method_response.tfer--k25cdk06uc-002F-eh1nmc-002F-GET-002F-301
   ]
 }
 
@@ -21,7 +22,8 @@ resource "aws_api_gateway_integration_response" "tfer--k25cdk06uc-002F-v0mdbf-00
   status_code = "200"
 
   depends_on = [
-    aws_api_gateway_integration.tfer--k25cdk06uc-002F-v0mdbf-002F-POST
+    aws_api_gateway_integration.tfer--k25cdk06uc-002F-v0mdbf-002F-POST,
+    aws_api_gateway_method_response.tfer--k25cdk06uc-002F-v0mdbf-002F-POST-002F-200
   ]
 
 }
